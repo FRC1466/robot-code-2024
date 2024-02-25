1f4329f0650f03018c9bbcd42aa77ac1a7157840
webbrobotics <45609921+webbrobotics@users.noreply.github.com>
2024-02-25 12:50:10 -0500

    Update PhotonCameraWrapper.java

--- src/main/java/frc/robot/subsystems/swervedrive/PhotonCameraWrapper.java
+++ src/main/java/frc/robot/subsystems/swervedrive/PhotonCameraWrapper.java
@@ -7,7 +7,8 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import frc.robot.Constants.Auton;
+import frc.robot.Constants;
+
 import java.io.IOException;
 import java.util.Optional;
 import org.photonvision.EstimatedRobotPose;
@@ -22,26 +23,22 @@ public class PhotonCameraWrapper {
 
   /** Create a new PhotonCameraWrapper to interface with photonvision camera. */
   public PhotonCameraWrapper() {
-    try {
-      aprilTagFieldLayout = AprilTagFields.k2023ChargedUp.loadAprilTagLayoutField();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    aprilTagFieldLayout = AprilTagFields.k2023ChargedUp.loadAprilTagLayoutField();
     aprilTagFieldLayout.setOrigin(
-        DriverStation.getAlliance() == Alliance.Blue
+        DriverStation.getAlliance().get() == Alliance.Blue
             ? OriginPosition.kBlueAllianceWallRightSide
             : OriginPosition.kRedAllianceWallRightSide);
 
-    var robotToCam = new Transform3d(Auton.cameraTranslation, Auton.cameraRotation);
+    var robotToCam = new Transform3d(Constants.cameraTranslation, Constants.cameraRotation);
     photonPoseEstimator =
         new PhotonPoseEstimator(
-            aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP, camera, robotToCam);
+            aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, camera, robotToCam);
     photonPoseEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
   }
 
   public void setAlliance() {
     aprilTagFieldLayout.setOrigin(
-        DriverStation.getAlliance() == Alliance.Blue
+        DriverStation.getAlliance().get() == Alliance.Blue
             ? OriginPosition.kBlueAllianceWallRightSide
             : OriginPosition.kRedAllianceWallRightSide);
     photonPoseEstimator.setFieldTags(aprilTagFieldLayout);
